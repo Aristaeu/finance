70ff10c95ce76ee123ded4f77ff4c185667845af
Amey Jadhav <158608733+Aristaeu@users.noreply.github.com>
2026-01-11 20:36:15 +0530

    Update app.py

--- Finance & Investments Landing Page/app.py
+++ Finance & Investments Landing Page/app.py
@@ -1,8 +1,17 @@
 import streamlit as st
+import os
 
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="Finance & Investments",
+    layout="wide"
+)
 
-with open("finance.html", "r", encoding="utf-8") as f:
+# Get current directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+html_path = os.path.join(BASE_DIR, "finance.html")
+
+# Load HTML
+with open(html_path, "r", encoding="utf-8") as f:
     html_content = f.read()
 
-st.components.v1.html(html_content, height=2000, scrolling=True)
+st.components.v1.html(html_content, height=3000, scrolling=True)
